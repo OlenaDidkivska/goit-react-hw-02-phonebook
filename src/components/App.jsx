54dcf04282e9bf1578d4da9cf229ace1c9789d40
OlenaDidkivska <elenatyutyuma@gmail.com>
2022-10-23 21:22:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,7 @@ import { PhonebookContainer } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
@@ -69,19 +64,25 @@ export class App extends Component {
       <PhonebookContainer>
         <h1>Phonebook</h1>
         <FormEl onSubmit={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.changeFilter} />
-        <ul>
-          {visibleContacts.map(contact => (
-            <Contacts
-              key={contact.id}
-              id={contact.id}
-              name={contact.name}
-              number={contact.number}
-              onDelete={this.deleteContact}
-            />
-          ))}
-        </ul>
+        {this.state.contacts.length !== 0 ? (
+          <>
+            <h2>Contacts</h2>
+            <Filter value={this.state.filter} onChange={this.changeFilter} />
+            <ul>
+              {visibleContacts.map(contact => (
+                <Contacts
+                  key={contact.id}
+                  id={contact.id}
+                  name={contact.name}
+                  number={contact.number}
+                  onDelete={this.deleteContact}
+                />
+              ))}
+            </ul>
+          </>
+        ) : (
+          ''
+        )}
       </PhonebookContainer>
     );
   }
